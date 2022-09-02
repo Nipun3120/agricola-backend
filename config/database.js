6d9c1b59da3715ff783bdac6b753410bd8644a5f
Nipun Verma <Nipunverma39@gmail.com>
2022-09-02 15:31:22 +0530

    db setup

---
+++ config/database.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+
+exports.connect = () => {
+  // Connecting to the database
+  mongoose
+    .connect(process.env.MONGO_URI || "mongodb://localhost:27017/capstone")
+    .then(() => console.log("Successfully connected to database"))
+    .catch((err) => {
+      console.log("Database Connection failed, exiting now...", "\n", err);
+      process.exit(1);
+    });
+};
---
+++ controllers/users.js
@@ -0,0 +1,6 @@
+const bcrypt = require("bcryptjs");
+const express = require("express");
+const router = express.Router();
+const User = require("../models/db/users");
+
+module.exports = router;
