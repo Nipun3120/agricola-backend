fa7906826a1484d5cd6ef518c7c878e6a5007551
Nipun Verma <Nipunverma39@gmail.com>
2022-09-04 23:56:50 +0530

    user profile fetch api

--- config/database.js
+++ config/database.js
@@ -6,7 +6,7 @@ exports.connect = () => {
     process.env.prod === true
       ? process.env.MONGO_URI
       : "mongodb://localhost:27017/capstone";
-
+  console.log(url);
   mongoose
     .connect(url)
     .then(() => console.log("Successfully connected to database"))
