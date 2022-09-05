b504207186e683c70540a719c53a14a56518731f
DhairyaSethi <55102840+DhairyaSethi@users.noreply.github.com>
2022-09-05 11:16:25 +0530

    approve requests

--- config/database.js
+++ config/database.js
@@ -1,17 +1,14 @@
 const mongoose = require("mongoose");
 
-exports.connect = () => {
+exports.connect = async () => {
   // Connecting to the database
   const url =
-    process.env.prod === true
-      ? process.env.MONGO_URI
-      : "mongodb://localhost:27017/capstone";
-
-  mongoose
+    process.env.MONGO_URI
+  await mongoose
     .connect(url)
-    .then(() => console.log("Successfully connected to database"))
     .catch((err) => {
       console.log("Database Connection failed, exiting now...", "\n", err);
       process.exit(1);
     });
+  console.log("Successfully connected to database")
 };
---
+++ controllers/approveRequests.js
@@ -0,0 +1,27 @@
+const router = require("express").Router();
+const ApproveRequest = require("../models/db/ApproveRequest");
+
+router.post("/toggle", async (req, res) => {
+  try {
+    const { address, approved } = req.body;
+    await ApproveRequest.findOneAndUpdate({ userAddress: address }, { approved })
+    res.status(200).json({
+      success: true
+    })
+  } catch (err) {
+    console.log("throwing error", err);
+    res.status(400).json({ success: false, message: "unable to toggle" });
+  }
+});
+
+router.post("/get-pending", async (_req, res) => {
+  try {
+    res.status(200).json({ success: true, result: await ApproveRequest.find({ approved: false }) })
+  } catch (err) {
+    console.log("throwing error", err);
+    res.status(400).json({ success: false, message: "unable to get pending" });
+  }
+});
+
+
+module.exports = router;
